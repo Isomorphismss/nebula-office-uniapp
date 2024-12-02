40937af35d3fe3c348a49897c9437e2bf9b452ee
Isomorphism <jw6699@nyu.edu>
2024-12-02 17:11:51 -0500

    added login page with styles for uni app

---
+++ pages/login/login.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<image src="../../static/logo-1.png" class="logo" mode="widthFix"></image>
+		<view class="logo-title">EMOS企业在线办公系统</view>
+		<view class="logo-subtitle">Ver 2050.2</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("login.less");
+</style>
