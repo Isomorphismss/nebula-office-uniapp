c9fa408484930879de74a2ccfd42ec477aef9fd0
Isomorphism <jw6699@nyu.edu>
2024-12-27 20:09:27 -0500

    fixed a small bug

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ app.$mount()
 let baseUrl = "http://127.0.0.1:8080/emos-wx-api"
 
 Vue.prototype.url={
-	register:baseUrl+"/user/register"
+	register:baseUrl+"/user/register",
+	login:baseUrl+"/user/login"
 }
 
 Vue.prototype.ajax=function(url,method,data,fun){
