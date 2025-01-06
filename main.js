6cf5e3dc7d3b7418ae9b36662f921cdf171b7c81
Isomorphism <jw6699@nyu.edu>
2025-01-06 16:03:09 -0500

    added mine page to display user info and useful links

--- main.js
+++ main.js
@@ -53,3 +53,15 @@ Vue.prototype.ajax=function(url,method,data,fun){
 		}
 	})
 }
+
+Vue.prototype.checkPermission = function(perms) {
+	let permission = uni.getStorageSync("permission")
+	let result = false;
+	for (let one of perms) {
+		if (permission.indexOf(one) != -1) {
+			result = true;
+			break;
+		}
+	}
+	return result;
+}
