2d1ef9e130bd678091a551b87d85bc2f12e2a7c6
Isomorphism <jw6699@nyu.edu>
2025-01-06 15:41:47 -0500

    imported the components to page

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -5,10 +5,18 @@
 </template>
 
 <script>
+	import uniList from '@/components/uni-list/uni-list.vue';
+	import uniListItem from '@/components/uni-list-item/uni-list-item.vue';
 	export default {
+		components: {
+			uniList,
+			uniListItem
+		},
 		data() {
 			return {
-				
+				name: '',
+				deptName: '',
+				photo: ''
 			}
 		},
 		methods: {
