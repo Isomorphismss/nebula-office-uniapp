4d7fffa91ba16a85594378a246c0431b6bf21ce5
Isomorphism <jw6699@nyu.edu>
2025-01-09 07:03:35 -0500

    added message page with style sheet to display the details of messages

---
+++ pages/message/message.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<view class="message">
+			<view class="desc">{{ sendTime }}</view>
+			<view class="opt">删除</view>
+		</view>
+		<view>{{ msg }}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("message.less");
+</style>
