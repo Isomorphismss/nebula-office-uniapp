ae09bd32629dfbe7c8069ec31e4c0fe6e1be389f
Isomorphism <jw6699@nyu.edu>
2025-01-09 06:54:39 -0500

    minor style change

--- pages/message_list/message_list.vue
+++ pages/message_list/message_list.vue
@@ -25,17 +25,7 @@
 			return {
 				page: 1,
 				length: 20,
-				list: [
-					{
-						id:"601027486863f742e88b1b22",
-						refId:"6010274d6863f742e88b1b23",
-						senderName:"系统消息",
-						senderPhoto:"https://static-1258386385.cos.ap-beijing.myqcloud.com/img/System.jpg",
-						msg:"HelloWorld",
-						readFlag:false,
-						sendTime:"2021-01-26 22:29:28"
-					}
-				],
+				list: [],
 				isLastPage: false
 			}
 		},
