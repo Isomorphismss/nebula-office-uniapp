110f50c5b5769e9ddb4288a04cf507bf3af243c8
Isomorphism <jw6699@nyu.edu>
2025-01-11 00:08:24 -0500

    added and registered components in meeting

--- pages/meeting/meeting.vue
+++ pages/meeting/meeting.vue
@@ -5,10 +5,31 @@
 </template>
 
 <script>
+	import uniPopup from '@/components/uni-popup/uni-popup.vue';
+	import uniPopupMessage from '@/components/uni-popup/uni-popup-message.vue';
+	import uniPopupDialog from '@/components/uni-popup/uni-popup-dialog.vue';
 	export default {
+		components: {
+			uniPopup,
+			uniPopupMessage,
+			uniPopupDialog
+		},
 		data() {
 			return {
-				
+				opt: null,
+				id: null,
+				uuid: null,
+				canEdit: true,
+				title: '',
+				date: '',
+				start: '',
+				end: '',
+				typeArray: ['线上会议', '线下会议'],
+				typeIndex: 0,
+				place: '',
+				desc: '会议内容',
+				members: [],
+				instanceId: null
 			}
 		},
 		methods: {
@@ -17,6 +38,6 @@
 	}
 </script>
 
-<style>
-
+<style lang="less">
+	@import url("meeting.less");
 </style>
