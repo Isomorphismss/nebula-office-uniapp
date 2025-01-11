f212c95014b310cb804be57d59fb8f2707867d4e
Isomorphism <jw6699@nyu.edu>
2025-01-11 15:49:13 -0500

    added template and style for the members page

--- pages/members/members.vue
+++ pages/members/members.vue
@@ -1,6 +1,14 @@
 <template>
-	<view>
-		
+	<view class="page">
+		<checkbox-group @change="selected">
+			<block v-for="dept in list" :key="dept.id">
+				<view class="list-title">{{ dept.deptName }}（{{ dept.count }}人）</view>
+				<view class="item" v-for="member in dept.members" :key="member.userId">
+					<view class="key">{{ member.name }}</view>
+					<checkbox class="value" :value="member.userId" :checked="member.checked"></checkbox>
+				</view>
+			</block>
+		</checkbox-group>
 	</view>
 </template>
 
