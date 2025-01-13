5083ed684438eea27e7b9981f7d638676fad9a09
Isomorphism <jw6699@nyu.edu>
2025-01-13 10:45:04 -0500

    added some frontend validation function before submitting the data to API

--- main.js
+++ main.js
@@ -97,3 +97,89 @@ Date.prototype.format = function(fmt) {
 	}
 	return fmt;
 }
+
+Vue.prototype.checkNull = function(data, name) {
+	if (data == null) {
+		uni.showToast({
+			icon: "none",
+			title: name + "不能为空"
+		})
+		return true
+	}
+	return false
+}
+
+Vue.prototype.checkBlank = function(data, name) {
+	if (data == null || data == "") {
+		uni.showToast({
+			icon: "none",
+			title: name + "不能为空"
+		})
+		return true
+	}
+	return false
+}
+
+Vue.prototype.checkValidName = function(data, name) {
+	if (data == null || data == "") {
+		uni.showToast({
+			icon: "none",
+			title: name + "不能为空"
+		})
+		return true
+	} else if (!/^[\u4e00-\u9fa5]{2,15}$/.test(data)) {
+		uni.showToast({
+			icon: "none",
+			title: name + "内容不正确"
+		})
+		return true
+	}
+	return false
+}
+
+Vue.prototype.checkValidTel = function(data, name) {
+	if (data == null || data == "") {
+		uni.showToast({
+			icon: "none",
+			title: name + "不能为空"
+		})
+		return true
+	} else if (!/^1[0-9]{10}$/.test(data)) {
+		uni.showToast({
+			icon: "none",
+			title: name + "内容不正确"
+		})
+		return true
+	}
+	return false
+}
+
+Vue.prototype.checkValidEmail = function(data, name) {
+	if (data == null || data == "") {
+		uni.showToast({
+			icon: "none",
+			title: name + "不能为空"
+		})
+		return true
+	} else if (!/^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/.test(data)) {
+		uni.showToast({
+			icon: "none",
+			title: name + "内容不正确"
+		})
+		return true
+	}
+	return false
+}
+
+Vue.prototype.checkValidStartAndEnd = function(start, end) {
+	let d1 = new Date("2000/01/01 " + start + ":00");
+	let d2 = new Date("2000/01/01 " + end + ":00");
+	if (d2.getTime() <= d1.getTime()) {
+		uni.showToast({
+			icon: "none",
+			title: "结束时间必须大于开始时间"
+		})
+		return true
+	}
+	return false
+}
