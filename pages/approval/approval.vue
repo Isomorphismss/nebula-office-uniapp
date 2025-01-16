7e11a5c4fd4d171580024549f3aa1623f0dcc633
Isomorphism <jw6699@nyu.edu>
2025-01-16 15:29:45 -0500

    added approval page with templates and styles

---
+++ pages/approval/approval.vue
@@ -0,0 +1,76 @@
+<template>
+	<view class="page" v-if="checkPermission(['WORKFLOW:APPROVAL'])">
+		<view class="meeting" v-if="processType == 'meeting'">
+			<view class="header">{{title}}</view>
+			<view class="attr">
+				<view class="list">
+					<view class="item">
+						<view class="key">日期</view>
+						<text class="value">{{ date }}</text>
+					</view>
+					<view class="item">
+						<view class="key">开始时间</view>
+						<text class="value">{{ start }}</text>
+					</view>
+					<view class="item">
+						<view class="key">结束时间</view>
+						<text class="value">{{ end }}</text>
+					</view>
+					<view class="item">
+						<view class="key">会议类型</view>
+						<text class="value">{{typeArray[typeIndex]}}</text>
+					</view>
+					<view class="item" v-if="typeArray[typeIndex] == '线下会议'">
+						<view class="key">地点</view>
+						<view class="value">{{ place }}</view>
+					</view>
+				</view>
+				<view>
+					<text class="desc">{{ desc }}</text>
+				</view>
+			</view>
+			<view class="members">
+				<view class="number">参会者（{{ members.length }}人）</view>
+				<view class="member">
+					<view class="user" v-for="one in members" :key="one.id">
+						<image :src="one.photo" mode="widthFix" class="photo"></image>
+						<text class="name">{{ one.name }}</text>
+					</view>
+				</view>
+			</view>
+			<block v-if="taskId!=null">
+				<button class="btn" @tap="approvalMeeting('同意')" type="primary">同意</button>
+				<button class="btn" @tap="approvalMeeting('不同意')" type="warn">不同意</button>
+			</block>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				processType: null,
+				taskId: null,
+				id: null,
+				uuid: null,
+				title: '',
+				date: '',
+				start: '',
+				end: '',
+				typeArray: ['在线会议', '线下会议'],
+				typeIndex: 0,
+				place: '',
+				desc: '',
+				members: []
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("approval.less");
+</style>
